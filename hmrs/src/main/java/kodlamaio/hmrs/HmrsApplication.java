d4231e2be50bf3ddd0883ec25f05b0de001233d7
Altug <huaweiyaltug@vodafone.local>
2022-02-11 11:15:41 +0300

    testing

--- hmrs/src/main/java/kodlamaio/hmrs/HmrsApplication.java
+++ hmrs/src/main/java/kodlamaio/hmrs/HmrsApplication.java
@@ -21,7 +21,7 @@ public class HmrsApplication {
 	    public Docket api() { 
 	        return new Docket(DocumentationType.SWAGGER_2)  
 	          .select()                                  
-	          .apis(RequestHandlerSelectors.basePackage("kodlamaio.hmrs"))                                  
+	          .apis(RequestHandlerSelectors.basePackage("testing.hmrs"))                                  
 	          .build();                                           
 	    }
 
